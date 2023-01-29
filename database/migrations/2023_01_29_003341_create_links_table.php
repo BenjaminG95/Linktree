79b9340fd4e843cb1408d301a6b80e4920dfa002
Benjamin <Galoisb95@gmail.com>
2023-01-29 03:01:45 +0100

    add migrations

---
+++ database/migrations/2023_01_29_003341_create_links_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('links', static function (Blueprint $table): void{
+            $table->id();
+            $table->string('name');
+            $table->text('link');
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
